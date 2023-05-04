86f209956b16bcdcf727c175ae55dc4ba6da25f9
DubanStevenCastilloMendoza <estivencastmen@gmail.com>
2023-05-04 02:45:13 +0200

    APIS terminadas

--- script.js
+++ script.js
@@ -1,19 +1,120 @@
-const pokemonIds = [1, 25, 133]; // IDs de los Pokemones que queremos obtener
+const containerCart = document.querySelector('#container-cart');
 
-pokemonIds.forEach(pokemonId => {
-  const url = `https://pokeapi.co/api/v2/pokemon/${pokemonId}/`;
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      // Aquí puedes procesar los datos del Pokemon en la variable "data"
-for(i = 1; i<=pokemonId; i++){
-    const name = document.querySelector('#f');
-    name.innerHTML = data.name
+
+function getPokemon(id) {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+  .then(res => res.json())
+  .then(data => crearCart(data))
 }
+
+function traerVarios(number) {
+  for(i=1; i <= number; i++){
+    getPokemon(i);
+  }
+}
+
+
+  function crearCart(pokemon) {
+    let contenedor = ""
    
-      console.log(`Nombre: ${data.name}`);
-      console.log(`Tipo(s): ${data.types.map(type => type.type.name).join(", ")}`);
-      console.log(`Imagen: ${data.sprites.front_default}`);
-    })
-    .catch(error => console.error(error));
-});
+      contenedor += `<div class="cart">
+      <img src="${pokemon.sprites.front_default}" alt="">
+      <div class="texto">
+          <h3 id="prueba">${pokemon.name}:</h3>
+          <p>${pokemon.abilities[1].ability.name}</p>
+          <p>${pokemon.moves[1].move.name}</p>
+      </div>
+  </div>`;
+  
+
+    containerCart.innerHTML += contenedor;
+  }
+
+
+
+traerVarios(6);
+
+
+
+
+
+const containerCart2 = document.querySelector('#container-cart-2');
+
+
+function getCharacterWar(id) {
+  fetch(`https://swapi.dev/api/people/${id}`)
+  .then(res => res.json())
+  .then(data => crearPerson(data))
+}
+
+
+
+ function traerWar(people) {
+   for(i=1; i <= people; i++){
+     getCharacterWar(i);
+   }
+ }
+
+
+   function crearPerson(person) {
+     let contenedor = ""
+   
+       contenedor += `<div class="cart">
+       <div class="texto">
+           <h3 id="prueba">${person.name}:</h3>
+           <p>Peso: ${person.height}</p>
+           <p>Masa: ${person.mass}</p>
+           <p>Color-Cabello: ${person.hair_color}</p>
+           <p>Color-Piel: ${person.skin_color}</p>
+           <p>Color-Ojos: ${person.eye_color}</p>
+           <p>Nacimiento: ${person.birth_year}</p>
+           <p>Género: ${person.gender}</p>
+       </div>
+   </div>`;
+  
+
+     containerCart2.innerHTML += contenedor;
+   }
+
+
+   traerWar(6)
+
+
+
+
+
+
+   const containerCart3 = document.querySelector('#container-cart-3');
+
+
+function getFrase(id) {
+  fetch(` https://api.breakingbadquotes.xyz/v1/quotes/${id}`)
+  .then(res => res.json())
+  .then(data => crearFrase(data))
+}
+
+
+
+  function traerFrase(fras) {
+    for(i=1; i <= fras; i++){
+      getFrase(i);
+    }
+  }
+
+
+    function crearFrase(frase) {
+      let contenedor = ""
+   
+        contenedor += `<div class="cart" style="height: 400px; width: 500px; pading:10px ; text-align: center;">
+        <div class="texto-p">
+            <h3 id="prueba" >${frase[0].author}</h3>
+            <p >${frase[0].quote}</p>
+        </div>
+    </div>`;
+  
+
+      containerCart3.innerHTML += contenedor;
+    }
+
+
+    traerFrase(6)
